636a985a283294d3cad3fe31f5a626060bff0e2a
ArunPratapSingh <180103642+ArunPratapSinghh@users.noreply.github.com>
2025-03-12 17:06:23 +0530

    Update and rename User_Information.ps1 to Members_in_DL.ps1

---
+++ Members_in_DL.ps1
@@ -0,0 +1,20 @@
+###User_Details in AD ###
+
+#enter the Name of DL
+$Name = Read-host "Enter the name of DL you want to find the members"
+
+#List of Group
+#$Name = Get-content -Path C:\temp\DL_list.txt
+#$Name = Import-CSV -path C:\temp\DL_List.csv
+
+foreach($DL in $Name)
+{
+  try{
+          #Member in DL
+          Get-adgroupmember -identity $Name |Select Name,SamAccountName
+  }catch{
+
+      Write-Host "DL not found in AD" -Forgroundcolor Red
+  }
+  
+}
