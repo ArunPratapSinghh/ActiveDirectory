d932f2f12edfeccd099503a62fd6b1919a34cace
ArunPratapSingh <180103642+ArunPratapSinghh@users.noreply.github.com>
2025-03-13 13:07:44 +0530

    Create AD_Group.ps1

---
+++ AD_Group.ps1
@@ -0,0 +1,7 @@
+################################################################################################
+#Title: This Script has been made to Create the AD Group in Active diretory using Powershell   #     
+# Date:                                                                                        #
+# Author: Arun Pratap Singh                                                                    #
+# Modified Date:                                                                               #
+################################################################################################
+
