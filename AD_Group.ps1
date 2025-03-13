880dda007909b93a9378c0a62657cc3f35a8308a
ArunPratapSingh <180103642+ArunPratapSinghh@users.noreply.github.com>
2025-03-13 13:26:45 +0530

    Update AD_Group.ps1
    Script_updated

--- AD_Group.ps1
+++ AD_Group.ps1
@@ -1,7 +1,26 @@
 ################################################################################################
 #Title: This Script has been made to Create the AD Group in Active diretory using Powershell   #     
-# Date:                                                                                        #
+# Date: 13 March 2024                                                                          #
 # Author: Arun Pratap Singh                                                                    #
-# Modified Date:                                                                               #
+# Modified Date: 13 March 2024                                                                 #
 ################################################################################################
 
+#Import Active Directory Module
+Import-Module ActiveDirectory
+
+#Location for AD group Path
+$OU_Path = ""
+
+#Import Files using txt file
+#$Group_List = Get-Content -Path D:\group_list.txt
+
+#Import File using csv file
+#$Group_List = Import-CSV -Path D:\group_list.csv
+
+foreach($Group in $Group_List)
+{
+      New-Adgroup -Name $Group.Name -Path $Path.Path -GroupScope Global -GroupCategory Security -Managedby $Manage.Manage
+      Write-Host "$Group.Name has been created" -ForgroundColor Green
+    
+}
+Write-Host "AD group has been created" -ForgroundColor Green
