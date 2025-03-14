3963db02514de6462fb59bcf4e9ac6ccdc42a33d
ArunPratapSingh <180103642+ArunPratapSinghh@users.noreply.github.com>
2025-03-14 13:48:34 +0530

    Update Computer_Object.ps1

--- Computer_Object.ps1
+++ Computer_Object.ps1
@@ -1,1 +1,27 @@
 
+################################################################################################
+#Title: This Script has been made for computer object in Active diretory using Powershell   #     
+# Date: 14 March 2024                                                                          #
+# Author: Arun Pratap Singh                                                                    #
+# Modified Date: 14 March 2024                                                                 #
+################################################################################################
+
+#Import Active Directory Module
+Import-Module ActiveDirectory
+
+#Location for AD group Path
+$OU_Path = ""
+
+#Import Files using txt file
+#$Group_List = Get-Content -Path D:\group_list.txt
+
+#Import File using csv file
+#$Group_List = Import-CSV -Path D:\group_list.csv
+
+foreach($Group in $Group_List)
+{
+      New-Adgroup -Name $Group.Name -Path $Path.Path -GroupScope Global -GroupCategory Security -Managedby $Manage.Manage
+      Write-Host "$Group.Name has been created" -ForgroundColor Green
+    
+}
+Write-Host "AD group has been created" -ForgroundColor Green
