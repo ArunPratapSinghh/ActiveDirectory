57068744bdea53971f05cf892b0de9b888e30267
ArunPratapSingh <180103642+ArunPratapSinghh@users.noreply.github.com>
2025-03-15 11:32:57 +0530

    Update Delete_AD_User.ps1

--- Delete_AD_User.ps1
+++ Delete_AD_User.ps1
@@ -1,1 +1,10 @@
 
+################################################################################################
+#Title: This Script has been made for Delete_AD_group in Active diretory using Powershell   #     
+# Date: 14 March 2024                                                                          #
+# Author: Arun Pratap Singh                                                                    #
+# Modified Date: 14 March 2024                                                                 #
+################################################################################################
+
+#Import Active Directory Module
+Import-Module ActiveDirectory
