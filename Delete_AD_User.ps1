5c5e43e5a4040926d5dce7d678e358fbc73d545a
ArunPratapSingh <180103642+ArunPratapSinghh@users.noreply.github.com>
2025-06-03 14:04:19 +0200

    Update Delete_AD_User.ps1

--- Delete_AD_User.ps1
+++ Delete_AD_User.ps1
@@ -3,8 +3,20 @@
 #Title: This Script has been made for Delete_AD_group in Active diretory using Powershell   #     
 # Date: 14 March 2024                                                                          #
 # Author: Arun Pratap Singh                                                                    #
-# Modified Date: 16 March 2024                                                                 #
+# Modified Date: 03 June 2025                                                                 #
 ################################################################################################
 
 #Import Active Directory Module
 Import-Module ActiveDirectory
+
+#list of servers
+$list = Get-content -Path C:\temp\server_list.txt
+
+foreach($Users in $list){
+
+    Get-Aduser -identity $User |Remove-Aduser -confirm:$false
+    Write-Host "$Users has been deleted from active Directory" -Forgroundcolor green
+
+}
+
+Write-Host "Script has been completed" -ForgroundColor Cyan 
